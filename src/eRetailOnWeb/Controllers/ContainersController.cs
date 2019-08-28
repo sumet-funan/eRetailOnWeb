8300061f46b2e55e365d9176166b539491689967
sumet-funan <sumet.funan@outlook.com>
2019-08-29 00:03:02 +0700

    Refactoring source code

--- src/eRetailOnWeb/Controllers/ContainersController.cs
+++ src/eRetailOnWeb/Controllers/ContainersController.cs
@@ -9,74 +9,8 @@ namespace eProductOnWeb.Controllers
 {
     public class ContainersController : Controller
     {
-        private static List<ContainerViewModel> _containerViewModelList = new List<ContainerViewModel>
-        {
-            new ContainerViewModel
-            {
-                Id = 1,
-                Name = "Container 1",
-                Description = "Container Description 1",
-            },
-            new ContainerViewModel
-            {
-                Id = 2,
-                Name = "Container 2",
-                Description = "Container Description 2",
-            },
-            new ContainerViewModel
-            {
-                Id = 3,
-                Name = "Container 3",
-                Description = "Container Description 3",
-            },
-            new ContainerViewModel
-            {
-                Id = 4,
-                Name = "Container 4",
-                Description = "Container Description 4",
-            },
-            new ContainerViewModel
-            {
-                Id = 5,
-                Name = "Container 5",
-                Description = "Container Description 5",
-            },
-            new ContainerViewModel
-            {
-                Id = 6,
-                Name = "Container 6",
-                Description = "Container Description 6",
-            },
-            new ContainerViewModel
-            {
-                Id = 7,
-                Name = "Container 7",
-                Description = "Container Description 7",
-            },
-            new ContainerViewModel
-            {
-                Id = 8,
-                Name = "Container 8",
-                Description = "Container Description 8",
-            },
-            new ContainerViewModel
-            {
-                Id = 9,
-                Name = "Container 9",
-                Description = "Container Description 9",
-            },
-            new ContainerViewModel
-            {
-                Id = 10,
-                Name = "Container 10",
-                Description = "Container Description 10",
-            },new ContainerViewModel
-            {
-                Id = 11,
-                Name = "Container 11",
-                Description = "Container Description 11",
-            },
-        };
+        private static List<ContainerViewModel> _containerViewModelList = GetContainerViewModelList();
+
 
         public IActionResult Index()
         {
@@ -97,6 +31,79 @@ namespace eProductOnWeb.Controllers
             var containerViewModel = _containerViewModelList.Find(x => x.Id == id);
             _containerViewModelList.Remove(containerViewModel);
             return View(nameof(Index), _containerViewModelList);
+        }
+
+        // private method
+        private static List<ContainerViewModel> GetContainerViewModelList()
+        {
+            return new List<ContainerViewModel>
+            {
+                new ContainerViewModel
+                {
+                    Id = 1,
+                    Name = "Container 1",
+                    Description = "Container Description 1",
+                },
+                new ContainerViewModel
+                {
+                    Id = 2,
+                    Name = "Container 2",
+                    Description = "Container Description 2",
+                },
+                new ContainerViewModel
+                {
+                    Id = 3,
+                    Name = "Container 3",
+                    Description = "Container Description 3",
+                },
+                new ContainerViewModel
+                {
+                    Id = 4,
+                    Name = "Container 4",
+                    Description = "Container Description 4",
+                },
+                new ContainerViewModel
+                {
+                    Id = 5,
+                    Name = "Container 5",
+                    Description = "Container Description 5",
+                },
+                new ContainerViewModel
+                {
+                    Id = 6,
+                    Name = "Container 6",
+                    Description = "Container Description 6",
+                },
+                new ContainerViewModel
+                {
+                    Id = 7,
+                    Name = "Container 7",
+                    Description = "Container Description 7",
+                },
+                new ContainerViewModel
+                {
+                    Id = 8,
+                    Name = "Container 8",
+                    Description = "Container Description 8",
+                },
+                new ContainerViewModel
+                {
+                    Id = 9,
+                    Name = "Container 9",
+                    Description = "Container Description 9",
+                },
+                new ContainerViewModel
+                {
+                    Id = 10,
+                    Name = "Container 10",
+                    Description = "Container Description 10",
+                },new ContainerViewModel
+                {
+                    Id = 11,
+                    Name = "Container 11",
+                    Description = "Container Description 11",
+                },
+            };
         }
     }
 }
