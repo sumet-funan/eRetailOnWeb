3ff11c1ec1bd3609f4f674a546e6fffaf373f59d
sumet-funan <sumet.funan@outlook.com>
2019-10-16 01:13:33 +0700

    Add underlords

---
+++ src/eRetailOnWeb/Models/Ranking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace eProductOnWeb.Models
+{
+    public class Ranking
+    {
+        [DataMember(Name = "dayOfWeek")]
+        public int DayOfWeek { get; set; }
+
+        [DataMember(Name = "day")]
+        public string Day { get; set; }
+
+        [DataMember(Name = "data")]
+        public List<RankingProfile> Data { get; set; }
+    }
+}
