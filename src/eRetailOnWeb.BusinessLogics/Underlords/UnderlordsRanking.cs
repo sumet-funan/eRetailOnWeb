96717c70a100ae818e1661679bc61b2552edd8b3
sfunan <sfunan@celestica.com>
2019-10-18 17:22:51 +0700

    [Underlords] Fix bug

---
+++ src/eRetailOnWeb.BusinessLogics/Underlords/UnderlordsRanking.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using eProductOnWeb.BusinessLogics.Storage.Container;
+using eProductOnWeb.Models.Underlords;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace eProductOnWeb.BusinessLogics.Underlords
+{
+    public class UnderlordsRanking
+    {
+        private IConfiguration _configuration;
+
+        private Blob _blob;
+
+        public UnderlordsRanking(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public Ranking GetUnderlordsWeekRanking(string containerName, string fileName)
+        {
+            _blob = new Blob(_configuration);
+            string contents = _blob.GetUnderlordsWeekRanking(containerName, fileName);
+            Ranking ranking = JsonConvert.DeserializeObject<Ranking>(contents);
+
+            WeekInformation weekInfoTotal = new WeekInformation
+            {
+                Day = "Total",
+                DayOfWeek = 99,
+                DayInformation = new List<DayInformation>()
+            };
+            WeekInformation weekInfo = ranking.WeekInformation.Where(x => x.DayOfWeek == 2).FirstOrDefault();
+            foreach (var dayInformation in weekInfo.DayInformation)
+            {
+                weekInfoTotal.DayInformation.Add(GetTotalPointOfWeek(ranking.WeekInformation, dayInformation.UserName));
+            };
+
+            ranking.WeekInformation.Add(weekInfoTotal);
+            return ranking;
+        }
+
+        private DayInformation GetTotalPointOfWeek(List<WeekInformation> weekInformations, string userName)
+        {
+            return new DayInformation
+            {
+                UserName = userName,
+                Point = GetTotalPoint(weekInformations, userName),
+            };
+        }
+
+        private double GetTotalPoint(List<WeekInformation> weekInformations, string userName)
+        {
+            double totalPoint = 0;
+            foreach (WeekInformation weekInformation in weekInformations)
+            {
+                if (weekInformation.MatchInformation.Officially)
+                {
+                    totalPoint += weekInformation.DayInformation.Where(x => x.UserName == userName).Select(x => x.Point).FirstOrDefault();
+                }
+            }
+            return totalPoint;
+        }
+    }
+}
--- src/eRetailOnWeb/Controllers/UnderlordsController.cs
+++ src/eRetailOnWeb/Controllers/UnderlordsController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Net.Http;
 using eProductOnWeb.BusinessLogics.Storage.Container;
+using eProductOnWeb.BusinessLogics.Underlords;
 using eProductOnWeb.Models.Underlords;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -12,7 +13,7 @@ namespace eProductOnWeb.Controllers
     public class UnderlordsController : Controller
     {
         private static HttpClient _client;
-        private Blob _blob;
+        private UnderlordsRanking _underlordsRanking;
         private readonly IConfiguration _configuration;
 
         public UnderlordsController(IConfiguration configuration)
@@ -26,49 +27,14 @@ namespace eProductOnWeb.Controllers
             string fileName = $"underlords-{year}{month}-{weekOfMonth}.json";
             string containerName = "week-information";
 
-            _blob = new Blob(_configuration);
-            string contents = _blob.GetUnderlordsWeekRanking(containerName, fileName);
-            Ranking result = JsonConvert.DeserializeObject<Ranking>(contents);
-
-            WeekInformation ranking = new WeekInformation
-            {
-                Day = "Total",
-                DayOfWeek = 99,
-                DayInformation = new List<DayInformation>
-                {
-                    GetTotalPointOfWeek(result.WeekInformation, "B"),
-                    GetTotalPointOfWeek(result.WeekInformation, "TOP"),
-                    GetTotalPointOfWeek(result.WeekInformation, "NEIL"),
-                    GetTotalPointOfWeek(result.WeekInformation, "OAT"),
-                    GetTotalPointOfWeek(result.WeekInformation, "NOT")
-                }
-            };
-            result.WeekInformation.Add(ranking);
+            _underlordsRanking = new UnderlordsRanking(_configuration);
+            Ranking result = _underlordsRanking.GetUnderlordsWeekRanking(containerName, fileName);
 
             if (result == null)
             {
                 result = new Ranking { WeekInformation = new List<WeekInformation> { new WeekInformation { DayInformation = new List<DayInformation>() } } };
             }
             return View(result);
-        }
-
-        private DayInformation GetTotalPointOfWeek(List<WeekInformation> weekInformations, string userName)
-        {
-            return new DayInformation
-            {
-                UserName = userName,
-                Point = GetTotalPoint(weekInformations, userName),
-            };
-        }
-
-        private double GetTotalPoint(List<WeekInformation> weekInformations, string userName)
-        {
-            double totalPoint = 0;
-            foreach (WeekInformation weekInformation in weekInformations)
-            {
-                totalPoint += weekInformation.DayInformation.Where(x => x.UserName == userName).Select(x => x.Point).FirstOrDefault();
-            }
-            return totalPoint;
         }
     }
 }
