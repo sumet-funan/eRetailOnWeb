4e3e6f8b01dd57b81b150c36110293b261744470
sfunan <sfunan@celestica.com>
2019-10-18 15:27:52 +0700

    [Underlords] Add blob logic

---
+++ src/eRetailOnWeb.BusinessLogics/Storage/Container/Blob.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eProductOnWeb.BusinessLogics.Storage.Container
+{
+    public class Blob
+    {
+        private string _connectionString = "DefaultEndpointsProtocol=https;AccountName=underlords;AccountKey=+jElAWf6bgRsSZAIlefQ8+43phYUtEXPfsAN7it6nyHStGv4V1i2TeKez+2qjMvLSH/hs3WbYJe2i14F4q5FgQ==;EndpointSuffix=core.windows.net";
+        private readonly IConfiguration _configuration;
+
+        public Blob(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetValue<string>("ConnectionStorageBlob");
+        }
+
+        public string GetUnderlordsWeekRanking(string containerName, string fileName)
+        {
+            if (CloudStorageAccount.TryParse(_connectionString, out CloudStorageAccount storageAccount))
+            {
+                var cloudBlobClient = storageAccount.CreateCloudBlobClient();
+                var cloudBlobContainer = cloudBlobClient.GetContainerReference(containerName);
+                var cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(fileName);
+                string contents = cloudBlockBlob.DownloadTextAsync().Result;
+                return contents;
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+        public void GetInformationFromStorageBlob()
+        {
+            ProcessAsync().GetAwaiter().GetResult();
+        }
+
+        private async Task ProcessAsync()
+        {
+            if (CloudStorageAccount.TryParse(_connectionString, out CloudStorageAccount storageAccount))
+            {
+                var cloudBlobClient = storageAccount.CreateCloudBlobClient();
+                var cloudBlobContainer = cloudBlobClient.GetContainerReference("week-rule");
+                await cloudBlobContainer.CreateIfNotExistsAsync();
+
+                var permissions = new BlobContainerPermissions
+                {
+                    PublicAccess = BlobContainerPublicAccessType.Blob
+                };
+                await cloudBlobContainer.SetPermissionsAsync(permissions);
+
+                var file = new FileStream(@"C:\Engr_CodeRepo\SourceCode\eRetailOnWeb\src\eRetailOnWeb\Files\rule-201910-3.json", FileMode.Open);
+                var cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference("rule-201910-3.json");
+                cloudBlockBlob.Properties.ContentType = "application/json";
+                await cloudBlockBlob.UploadFromStreamAsync(file);
+                file.Close();
+            }
+            else
+            {
+                Console.WriteLine("The connection string isn't valid");
+                Console.WriteLine("Press any key to exit the application.");
+            }
+        }
+    }
+}
--- src/eRetailOnWeb/Controllers/UnderlordsController.cs
+++ src/eRetailOnWeb/Controllers/UnderlordsController.cs
@@ -1,99 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Threading.Tasks;
-using eProductOnWeb.Models;
+using eProductOnWeb.BusinessLogics.Storage.Container;
 using eProductOnWeb.Models.Underlords;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.Storage;
-using Microsoft.Azure.Storage.Blob;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace eProductOnWeb.Controllers
 {
     public class UnderlordsController : Controller
     {
-        private static HttpClient _client = new HttpClient();
+        private static HttpClient _client;
+        private Blob _blob;
+        private readonly IConfiguration _configuration;
 
-        public async Task<IActionResult> Index(int year, int month, int weekOfMonth)
+        public UnderlordsController(IConfiguration configuration)
         {
-            string endpoint = @"https://eea963a2-cb0d-4f72-82a9-73650417333c.mock.pstmn.io/underlords/get";
-            string queryString = $"?year={year}&month={month}&weekOfMonth={weekOfMonth}";
-            string url = endpoint + queryString;
-            HttpResponseMessage response = await _client.GetAsync(url);
-            if (response.IsSuccessStatusCode)
-            {
-                var result = await response.Content.ReadAsAsync<Ranking>();
+            _client = new HttpClient();
+            _configuration = configuration;
+        }
 
-                WeekInformation ranking = new WeekInformation
+        public IActionResult Index(int year, int month, int weekOfMonth)
+        {
+            string fileName = $"underlords-{year}{month}-{weekOfMonth}.json";
+            string containerName = "week-information";
+
+            _blob = new Blob(_configuration);
+            string contents = _blob.GetUnderlordsWeekRanking(containerName, fileName);
+            Ranking result = JsonConvert.DeserializeObject<Ranking>(contents);
+
+            WeekInformation ranking = new WeekInformation
+            {
+                Day = "Total",
+                DayOfWeek = 99,
+                DayInformation = new List<DayInformation>
                 {
-                    Day = "Total",
-                    DayOfWeek = 99,
-                    DayInformation = new List<DayInformation>
-                    {
-                        CreateDayInformation(result.WeekInformation, "B"),
-                        CreateDayInformation(result.WeekInformation, "TOP"),
-                        CreateDayInformation(result.WeekInformation, "NEIL"),
-                        CreateDayInformation(result.WeekInformation, "OAT"),
-                        CreateDayInformation(result.WeekInformation, "NOT")
-                    }
-                };
-                result.WeekInformation.Add(ranking);
+                    GetTotalPointOfWeek(result.WeekInformation, "B"),
+                    GetTotalPointOfWeek(result.WeekInformation, "TOP"),
+                    GetTotalPointOfWeek(result.WeekInformation, "NEIL"),
+                    GetTotalPointOfWeek(result.WeekInformation, "OAT"),
+                    GetTotalPointOfWeek(result.WeekInformation, "NOT")
+                }
+            };
+            result.WeekInformation.Add(ranking);
 
-                GetInformationFromStorageBlob();
-
-                return View(result);
-            }
-
-            Ranking rankings = new Ranking { WeekInformation = new List<WeekInformation> { new WeekInformation { DayInformation = new List<DayInformation>() } } };
-            return View(rankings);
-        }
-
-        private void GetInformationFromStorageBlob()
-        {
-            ProcessAsync().GetAwaiter().GetResult();
-        }
-
-        private static async Task ProcessAsync()
-        {
-            var connectionString = "DefaultEndpointsProtocol=https;AccountName=underlords;AccountKey=+jElAWf6bgRsSZAIlefQ8+43phYUtEXPfsAN7it6nyHStGv4V1i2TeKez+2qjMvLSH/hs3WbYJe2i14F4q5FgQ==;EndpointSuffix=core.windows.net";
-
-            if (CloudStorageAccount.TryParse(connectionString, out CloudStorageAccount storageAccount))
+            if (result == null)
             {
-                var cloudBlobClient = storageAccount.CreateCloudBlobClient();
-                var cloudBlobContainer = cloudBlobClient.GetContainerReference("week-rule");
-                await cloudBlobContainer.CreateIfNotExistsAsync();
-
-                var permissions = new BlobContainerPermissions
-                {
-                    PublicAccess = BlobContainerPublicAccessType.Blob
-                };
-                await cloudBlobContainer.SetPermissionsAsync(permissions);
-
-                var file = new FileStream(@"C:\Engr_CodeRepo\SourceCode\eRetailOnWeb\src\eRetailOnWeb\Files\rule-201910-3.json", FileMode.Open);
-                var cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference("rule-201910-3.json");
-                cloudBlockBlob.Properties.ContentType = "application/json";
-                await cloudBlockBlob.UploadFromStreamAsync(file);
-                file.Close();
+                result = new Ranking { WeekInformation = new List<WeekInformation> { new WeekInformation { DayInformation = new List<DayInformation>() } } };
             }
-            else
-            {
-                Console.WriteLine("The connection string isn't valid");
-                Console.WriteLine("Press any key to exit the application.");
-            }
+            return View(result);
         }
 
-        private DayInformation CreateDayInformation(List<WeekInformation> weekInformations, string userName)
+        private DayInformation GetTotalPointOfWeek(List<WeekInformation> weekInformations, string userName)
         {
             return new DayInformation
             {
                 UserName = userName,
-                Point = GetPointTatal(weekInformations, userName),
+                Point = GetTotalPoint(weekInformations, userName),
             };
         }
 
-        private double GetPointTatal(List<WeekInformation> weekInformations, string userName)
+        private double GetTotalPoint(List<WeekInformation> weekInformations, string userName)
         {
             double totalPoint = 0;
             foreach (WeekInformation weekInformation in weekInformations)
